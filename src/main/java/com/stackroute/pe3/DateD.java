55e912320311d6b23ef2cf0267b5d6fb38474904
gaurav714 <gauravnautiyal77@gmail.com>
2018-08-09 09:36:45 +0530

    updated date

--- src/main/java/com/stackroute/pe3/DateD.java
+++ src/main/java/com/stackroute/pe3/DateD.java
@@ -8,16 +8,17 @@ public class DateD {
 	public String dateCal()	
 	{
 		Date date= new Date();
-		Date d1=new Date(date.getTime()-2*(24*3600*1000));
+		Date d1=new Date(date.getTime()-3*(24*3600*1000));
 		String d="E dd/MM/yyyy";
 		SimpleDateFormat f=new SimpleDateFormat(d);
 		String s1=f.format(d1);
 		System.out.println(d1);
-		Date d2=new Date(date.getTime()-4*(24*3600*1000));
+		Date d2=new Date(date.getTime()+3*(24*3600*1000));
 		String s2=f.format(d2);
 		System.out.println(s2);
 		String s="";
 		s=s+s1;
+		s=s+" ";
 		s=s+s2;
 		return s;
 	}
